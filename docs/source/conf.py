91569994e72fa7dc7f44f78066981fa48fa1e775
MBernAca <85155827+MBernAca@users.noreply.github.com>
2022-02-17 09:39:16 +0100

    17/02/2022

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'PecubeGUI'
+copyright = '2022, Maxime Bernard'
+author = 'Maxime Bernard'
 
 release = '0.1'
 version = '0.1.0'
