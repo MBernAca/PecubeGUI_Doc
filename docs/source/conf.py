5749d14402702224e3a3f377743a7d8be9201acb
MBernAca <85155827+MBernAca@users.noreply.github.com>
2024-03-13 16:13:47 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,7 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'sphinx-rtd-theme'
+#html_theme = 'sphinx-rtd-theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
